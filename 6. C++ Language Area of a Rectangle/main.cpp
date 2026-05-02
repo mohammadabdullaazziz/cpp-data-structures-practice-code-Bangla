a5352d638fddf97f628b5b3edd60bb10435803d1
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2026-05-02 22:11:05 +0600

    Added Swap Integer Number

--- 6. C++ Language Area of a Rectangle/main.cpp
+++ 6. C++ Language Area of a Rectangle/main.cpp
@@ -13,3 +13,49 @@ int main(){
    cout << "Width is: " << width << "\n";
    cout << "Area of the rectangle is: " << area << "\n";
 }
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+     int num1, num2, num3;
+     num1 = 10, num2 = 20;
+     
+     cout<< "Num1 = " <<num1 <<endl;
+     cout<< "Num2 = " <<num2 <<endl;
+
+     num3 = num2;
+     num2 = num1;
+     num1 = num3;
+
+     cout << "Num1 = " << num1 << endl;
+     cout << "Num2 = " << num2 << endl;
+     return 0;
+}
+
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+     int num1, num2, num3;
+     
+     cout<< "Enter first integer number: ";
+     cin>> num1;
+
+     cout << "Enter second integer number: ";
+     cin >> num2;
+     
+     cout<< "Num1 is: " <<num1 <<endl;
+     cout<< "Num2 is: " <<num2 <<endl;
+
+     num3 = num2;
+     num2 = num1;
+     num1 = num3;
+
+     cout << "Num1 = " << num1 << endl;
+     cout << "Num2 = " << num2 << endl;
+     return 0;
+}
