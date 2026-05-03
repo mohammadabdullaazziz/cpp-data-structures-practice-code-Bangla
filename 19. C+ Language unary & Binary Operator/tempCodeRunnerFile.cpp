4bfb63e2e51146778939c43f4401461ec58fc569
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2026-05-03 18:04:08 +0600

    C+ Language unary & Binary Operator

---
+++ 19. C+ Language unary & Binary Operator/tempCodeRunnerFile.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+// #include <string>
+// using namespace std;
+// int main(){
+//      int num1, num2, num3;
+//      num1 = 10, num2 = 20;
+     
+//      cout<< "Num1 = " <<num1 <<endl;
+//      cout<< "Num2 = " <<num2 <<endl;
+
+//      num3 = num2;
+//      num2 = num1;
+//      num1 = num3;
+
+//      cout << "Num1 = " << num1 << endl;
+//      cout << "Num2 = " << num2 << endl;
+//      return 0;
+// }
